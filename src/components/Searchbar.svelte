3bcff3c48fb3c3b6483cf6a95faf7c2afe82f727
lnappsdev <lightning-faucet@protonmail.com>
2020-04-27 19:11:00 +0100

    create Searchbar component

---
+++ src/components/Searchbar.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { goto } from '@sapper/app'
+  import { searchResults, showOverlay } from '../store'
+  import { makeApiRequest, globalErrorHandler } from '../components/create-api'
+
+  let q = '';
+
+  async function doSearch(){
+    let res = await makeApiRequest('/search/posts?q='+encodeURIComponent(q), null, { method: 'GET' })
+      .catch(console.error);
+
+    searchResults.set(res);
+    showOverlay.set(false);
+    return goto('/search')
+  }
+</script>
+<style>
+  .search {
+    max-width: 200px;
+    margin: 0;
+  }
+  #search-button {
+    margin-left: 0;
+    padding-left: 1.3em;
+    padding-right: 1.3em
+  }
+</style>
+
+<input class="search" type="text" bind:value={q}>
+<button id="search-button" on:click|preventDefault={doSearch}>Search</button>
