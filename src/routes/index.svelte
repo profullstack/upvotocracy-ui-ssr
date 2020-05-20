3ccf4aefe97936a61255ff2bbb8f3e21b3bffc26
Anthony Ettinger <anthony@chovy.com>
2020-05-20 15:11:57 -0700

    fixups

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,29 +2,29 @@
   export async function preload(page, session) {
     //const { category } = page.params;
 
-    let url = 'API_BASE_URL'
-    let headers
-    let username
-    let sort
-    let type
-    let pageNumber = 0
+    let url = "API_BASE_URL";
+    let headers;
+    let username;
+    let sort;
+    let type;
+    let pageNumber = 0;
 
-    if (page.query.sort) type = page.query.sort
-    if (page.query.page) pageNumber = parseInt(page.query.page)
+    if (page.query.sort) type = page.query.sort;
+    if (page.query.page) pageNumber = parseInt(page.query.page);
 
-    if (type === 'hot') {
-      sort = '-rank'
-    } else if (type === 'top') {
-      sort = '-score'
-    } else if (type === 'new') {
-      sort = '-created'
-    } else if (type === 'comments') {
-      sort = 'comments';
-    } else if (type === 'not') {
-      sort = '+score';
+    if (type === "hot") {
+      sort = "-rank";
+    } else if (type === "top") {
+      sort = "-score";
+    } else if (type === "new") {
+      sort = "-created";
+    } else if (type === "comments") {
+      sort = "comments";
+    } else if (type === "not") {
+      sort = "+score";
     }
 
-    url += `/posts/?sort=${sort}&page=${pageNumber}`
+    url += `/posts/?sort=${sort}&page=${pageNumber}`;
 
     let res = await this.fetch(url);
     res = await res.json();
@@ -34,8 +34,9 @@
     return { posts, page, morePosts, sort };
   }
 </script>
+
 <script>
-  import Home from '../components/Home.svelte'
+  import Home from "../components/Home.svelte";
   export let posts;
   export let page;
   export let morePosts;
@@ -43,7 +44,7 @@
 </script>
 
 <svelte:head>
-  <title>SITE_DOMAIN</title>
+  <title>SITE_DESCRIPTION</title>
 </svelte:head>
 
-<Home posts={posts} page={page} morePosts={morePosts} sort={sort}/>
+<Home {posts} {page} {morePosts} {sort} />
