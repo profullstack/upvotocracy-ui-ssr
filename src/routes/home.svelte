026506903956cc10c932da89bc6b99a447539696
Anthony Ettinger <anthony@chovy.com>
2020-05-21 10:21:56 -0700

    prettier

--- src/routes/home.svelte
+++ src/routes/home.svelte
@@ -5,9 +5,9 @@
 </script>
 
 <script>
-  import Home from "../components/Home.svelte";
-  import { onMount } from "svelte";
-  import { makeApiRequest, globalErrorHandler } from "../components/create-api";
+  import Home from '../components/Home.svelte';
+  import { onMount } from 'svelte';
+  import { makeApiRequest, globalErrorHandler } from '../components/create-api';
 
   export let page;
   let morePosts;
@@ -19,27 +19,26 @@
     let pageNumber;
     let currentURL = new URLSearchParams(window.location.search);
 
-    if (currentURL.get("page")) pageNumber = parseInt(currentURL.get("page"));
+    if (currentURL.get('page')) pageNumber = parseInt(currentURL.get('page'));
     else pageNumber = 0;
 
     if (page.query.sort) type = page.query.sort;
 
-    if (type === "hot") {
-      sort = "-rank";
-    } else if (type === "top") {
-      sort = "-score";
-    } else if (type === "new") {
-      sort = "-created";
-    } else if (type === "comments") {
-      sort = "comments";
-    } else if (type === "not") {
-      sort = "+score";
+    if (type === 'hot') {
+      sort = '-rank';
+    } else if (type === 'top') {
+      sort = '-score';
+    } else if (type === 'new') {
+      sort = '-created';
+    } else if (type === 'comments') {
+      sort = 'comments';
+      sort = '+score';
     }
 
     let url = `/subscriptions?sort=${sort}&page=${pageNumber}`;
 
-    const res = await makeApiRequest(url, null, { method: "GET" }).catch(err =>
-      globalErrorHandler(err)
+    const res = await makeApiRequest(url, null, { method: 'GET' }).catch(err =>
+      globalErrorHandler(err),
     );
 
     if (!res) return;
@@ -54,7 +53,7 @@
     fetchPage();
   });
 
-  $: if (typeof window != "undefined") fetchPage(page.query);
+  $: if (typeof window != 'undefined') fetchPage(page.query);
 </script>
 
 <svelte:head>
