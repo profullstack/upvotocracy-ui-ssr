d45d0698eb261d0e4cb0f5205fb7a229ed3818ea
Anthony Ettinger <anthony@chovy.com>
2020-05-21 10:22:29 -0700

    Coupondeals (#19)
    * package
    
    * fixups
    
    * sample
    
    * prettier

--- src/components/Comment.svelte
+++ src/components/Comment.svelte
@@ -1,91 +1,95 @@
 <script>
-  import { createEventDispatcher, onMount } from 'svelte'
-  import moment from 'moment'
-  import { userStore } from '../store'
-  import { parseContent } from '../utils/parseContent'
-  import { makeApiRequest, globalErrorHandler } from '../components/create-api'
-  
-  export let id, comments
-  const dispatch = createEventDispatcher()
+  import { createEventDispatcher, onMount } from "svelte";
+  import moment from "moment";
+  import { userStore } from "../store";
+  import { parseContent } from "../utils/parseContent";
+  import { makeApiRequest, globalErrorHandler } from "../components/create-api";
 
-  let user = {}
+  export let id, comments;
+  const dispatch = createEventDispatcher();
+
+  let user = {};
   userStore.subscribe(value => {
-    if (value) user = value
-  })
+    if (value) user = value;
+  });
 
-  const removeComment = async (event) => {
-    event.preventDefault()
-    const { srcElement: { id: commentId } } = event
+  const removeComment = async event => {
+    event.preventDefault();
+    const {
+      srcElement: { id: commentId }
+    } = event;
 
-    const url = `/post/${id}/${commentId}`
+    const url = `/post/${id}/${commentId}`;
 
-    const post = await makeApiRequest(url, null, { method: 'DELETE' })
-      .catch(err => globalErrorHandler(err))
+    const post = await makeApiRequest(url, null, { method: "DELETE" }).catch(
+      err => globalErrorHandler(err)
+    );
 
-    if (!post) return
+    if (!post) return;
 
-    dispatch('update-comment', post)
-  }
+    dispatch("update-comment", post);
+  };
 
   const voteComment = async (comment, state) => {
-    const url = `/post/${id}/${comment.id}/${state}`
+    const url = `/post/${id}/${comment.id}/${state}`;
 
-    const data = await makeApiRequest(url, null, { method: 'GET' })
-      .catch(err => globalErrorHandler(err))
+    const data = await makeApiRequest(url, null, { method: "GET" }).catch(err =>
+      globalErrorHandler(err)
+    );
 
-    if (!data) return
-    
+    if (!data) return;
+
     for (let i = 0; i < comments.length; i += 1) {
-      console.log(comments[i])
+      console.log(comments[i]);
       if (comments[i].id == comment.id) {
         comments[i].score = data.score;
         break;
       }
-    } 
-  }
+    }
+  };
 
-  let highlighted
+  let highlighted;
 
   const highlightComment = async () => {
-    const url = window.location.href
-    const split = url.split('#')
-    highlighted = split[1]
-    if (document.getElementById(highlighted)) document.getElementById(highlighted).scrollIntoView()
-  }
+    const url = window.location.href;
+    const split = url.split("#");
+    highlighted = split[1];
+    if (document.getElementById(highlighted))
+      document.getElementById(highlighted).scrollIntoView();
+  };
 
   onMount(() => {
-    highlightComment()
-    sortComments(sort)
-  })
+    highlightComment();
+    sortComments(sort);
+  });
 
-  let sort = 'new'
+  let sort = "new";
 
   const sortComments = () => {
-    if (sort == 'top') {
-      comments = comments.sort((a, b) => {      
+    if (sort == "top") {
+      comments = comments.sort((a, b) => {
         return b.score - a.score;
       });
     }
 
-    if (sort == 'new') {
+    if (sort == "new") {
       comments = comments.sort((a, b) => {
-        b.created = new Date(b.created)
-        a.created = new Date(a.created)
+        b.created = new Date(b.created);
+        a.created = new Date(a.created);
         return b.created - a.created;
       });
     }
 
-    if (sort == 'original') {
+    if (sort == "original") {
       comments = comments.sort((a, b) => {
-        b.created = new Date(b.created)
-        a.created = new Date(a.created)
+        b.created = new Date(b.created);
+        a.created = new Date(a.created);
         return a.created - b.created;
       });
     }
-}
+  };
 
-  $: sortComments(sort)
-
+  $: sortComments(sort);
 </script>
 
 <style>
@@ -111,7 +115,7 @@
     display: flex;
     flex-direction: column;
     align-items: center;
-    padding: .3em 0;
+    padding: 0.3em 0;
     font-size: 14px;
     line-height: 1.5em;
     font-weight: 400;
@@ -131,33 +135,53 @@
     width: 100%;
   }
   .sort {
-    margin-bottom: .5em;
+    margin-bottom: 0.5em;
   }
-
 </style>
+
 {#if comments.length > 0}
   <div class="sort">
-    <a href="javascript:void(0)" on:click={() => sort = 'top'}>Top</a> . 
-    <a href="javascript:void(0)" on:click={() => sort = 'new'}>New</a> .
-    <a href="javascript:void(0)" on:click={() => sort = 'original'}>Original</a>
+    <a href="javascript:void(0)" on:click={() => (sort = 'top')}>Top</a>
+    .
+    <a href="javascript:void(0)" on:click={() => (sort = 'new')}>New</a>
+    .
+    <a href="javascript:void(0)" on:click={() => (sort = 'original')}>
+      Original
+    </a>
   </div>
 {/if}
 <div class="content">
   {#each comments as comment}
-     <div class="voting-container">
-      <span class="upvote-button"><span on:click={ () => voteComment(comment, 'upvote') }>❮</span></span>
+    <div class="voting-container">
+      <span class="upvote-button">
+        <span on:click={() => voteComment(comment, 'upvote')}>❮</span>
+      </span>
       <span>{comment.score}</span>
-      <span class="downvote-button"><span on:click={ () => voteComment(comment, 'downvote') }>❯</span></span>  
+      <span class="downvote-button">
+        <span on:click={() => voteComment(comment, 'downvote')}>❯</span>
+      </span>
     </div>
-    <div class="comment-container" id="comment-id-{comment.id}" class:comment-highlighted={highlighted === `comment-id-${comment.id}`}>   
+    <div
+      class="comment-container"
+      id="comment-id-{comment.id}"
+      class:comment-highlighted={highlighted === `comment-id-${comment.id}`}>
       <div class="comment-metadata">
-        <a href={`/u/${comment.author.username}`}>{ comment.author.username }</a> · <span>{ moment(comment.created).fromNow() }</span>
-        {#if comment.author.id === user.id }
-          <span id={ comment.id } class="remove-button float-right" on:click={ removeComment }>Delete</span>
+        <a href={`/u/${comment.author.username}`}>{comment.author.username}</a>
+        ·
+        <span>{moment(comment.created).fromNow()}</span>
+        {#if comment.author.id === user.id}
+          <span
+            id={comment.id}
+            class="remove-button float-right"
+            on:click={removeComment}>
+            Delete
+          </span>
         {/if}
       </div>
       <div class="comment-body">
-        <span>{@html parseContent(comment.body) }</span>
+        <span>
+          {@html parseContent(comment.body)}
+        </span>
       </div>
     </div>
   {/each}
--- src/routes/home.svelte
+++ src/routes/home.svelte
@@ -5,59 +5,59 @@
 </script>
 
 <script>
-  import Home from '../components/Home.svelte'
-  import { onMount } from 'svelte'
-  import { makeApiRequest, globalErrorHandler } from '../components/create-api'
-  
+  import Home from '../components/Home.svelte';
+  import { onMount } from 'svelte';
+  import { makeApiRequest, globalErrorHandler } from '../components/create-api';
+
   export let page;
   let morePosts;
   let posts;
   let sort;
 
   const fetchPage = async () => {
-    let type
-    let pageNumber
-    let currentURL = new URLSearchParams(window.location.search)
+    let type;
+    let pageNumber;
+    let currentURL = new URLSearchParams(window.location.search);
 
-    if (currentURL.get('page')) pageNumber = parseInt(currentURL.get('page'))
-    else pageNumber = 0
+    if (currentURL.get('page')) pageNumber = parseInt(currentURL.get('page'));
+    else pageNumber = 0;
 
-    if (page.query.sort) type = page.query.sort 
+    if (page.query.sort) type = page.query.sort;
 
     if (type === 'hot') {
-      sort = '-rank'
+      sort = '-rank';
     } else if (type === 'top') {
-      sort = '-score'
+      sort = '-score';
     } else if (type === 'new') {
-      sort = '-created'
+      sort = '-created';
     } else if (type === 'comments') {
       sort = 'comments';
-    } else if (type === 'not') {
       sort = '+score';
     }
 
     let url = `/subscriptions?sort=${sort}&page=${pageNumber}`;
 
-    const res = await makeApiRequest(url, null, { method: 'GET' })
-      .catch(err => globalErrorHandler(err))
+    const res = await makeApiRequest(url, null, { method: 'GET' }).catch(err =>
+      globalErrorHandler(err),
+    );
 
-    if (!res) return
+    if (!res) return;
 
     posts = res.posts;
     morePosts = res.more;
 
     return { posts, page, morePosts, sort };
-  }
+  };
 
   onMount(async () => {
-    fetchPage()
-  })
+    fetchPage();
+  });
 
-  $: if (typeof window != 'undefined') fetchPage(page.query)
+  $: if (typeof window != 'undefined') fetchPage(page.query);
 </script>
 
 <svelte:head>
- <title>SITE_DOMAIN</title>
+  <title>SITE_DESCRIPTION</title>
 </svelte:head>
 
-<Home posts={posts} page={page} morePosts={morePosts} sort={sort} subscriptions={true}/>
+<Home {posts} {page} {morePosts} {sort} subscriptions={true} />
