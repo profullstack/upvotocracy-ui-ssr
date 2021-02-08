4f7a941cae055f30ef2ab72a5d9083a7638c3d08
lnappsdev <lightning-faucet@protonmail.com>
2021-02-08 17:30:10 +0000

    highlight sponsored posts

--- TODO.md
+++ TODO.md
@@ -2,11 +2,11 @@
 
 - show account age on account summary
 - squished text in messaging window (see screenshot)
-- add leaderboard and footer links back
+- ~~add leaderboard and footer links back~~
 - creating post doesn't redirect to post
-- call out sponsored posts
+- ~~call out sponsored posts~~
 - no way to create new category
 - mobile topnav is squished
-- need sponsored post payment page
+- ~~need sponsored post payment page~~
 - on post page, title is below content, should be above
 - subnav at top is missing, should follow user around: new, top, etc
--- src/components/Post.svelte
+++ src/components/Post.svelte
@@ -107,7 +107,7 @@
       {post.score}
       <VoteArrow type="down" click={downvote} selected={userVote == 'downvote'} />
     </div>
-    <div class="post-inner-container">
+    <div class:sponsored={post.sponsored} class="post-inner-container">
       {#if (postThumb && !withDetails) || (postThumb && withDetails && !postVideo)}
         <div class="post-thumb" style={`background-image: url(${postThumb})`} />
       {/if}
@@ -206,6 +206,9 @@
               </svg>
               {moment(post.created).fromNow()}
             </span>
+            {#if post.sponsored}
+              <a rel="prefetch" href="/sponsor" class="sponsored-txt">sponsored</a>
+            {/if}
           </div>
         </div>
       </div>
@@ -217,6 +220,14 @@
 </div>
 
 <style>
+  .sponsored {
+    border: 1px solid var(--green-accent) !important;
+  }
+  .sponsored-txt {
+    font-size: small;
+    margin-left: 5px;
+    color: var(--green-accent);
+  }
   .container {
     max-width: 750px;
     flex-grow: 1;
