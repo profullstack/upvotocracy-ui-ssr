49918a9f5daa3f6a050589f69b73c3c64ea7f730
Anthony Ettinger <anthony@chovy.com>
2023-08-07 05:12:39 -0700

    terms

---
+++ src/routes/privacy.svelte
@@ -0,0 +1,58 @@
+<script>
+  const domain = 'SITE_DOMAIN';
+  const email = 'SITE_EMAIL';
+</script>
+
+<section>
+
+<h1>Privacy Policy for {domain}</h1>
+
+<p>Effective Date: August 7, 2023</p>
+
+<h2>1. Introduction</h2>
+<p>
+  We at {domain} take your privacy seriously. This privacy policy explains what personal
+  information we collect, why we collect it, and how we use it.
+</p>
+
+<h2>2. What Information We Collect</h2>
+<p>
+  We may collect information like your name, email address, IP address, and other relevant details
+  when you use our services or visit our site.
+</p>
+
+<h2>3. How We Use Your Information</h2>
+<p>
+  We use the information to provide, improve, and maintain our services, respond to your requests,
+  and communicate with you about our products and services.
+</p>
+
+<h2>4. How We Share Your Information</h2>
+<p>
+  We do not sell or share your personal information with third parties except as described in this
+  policy or with your consent.
+</p>
+
+<h2>5. Cookies</h2>
+<p>
+  We use cookies to understand how you use our site and to improve your experience. You can control
+  your cookie preferences in your browser settings.
+</p>
+
+<h2>6. Your Choices</h2>
+<p>
+  You may access, correct, or delete your personal information by contacting us at
+  <a href="mailto:{email}?subject={domain}">{email}</a>.
+</p>
+
+<h2>7. Changes to This Policy</h2>
+<p>
+  We may update this policy from time to time. Any changes will be posted on this page, so please
+  check back regularly.
+</p>
+
+<h2>8. Contact Us</h2>
+<p>
+  If you have any questions or concerns about this policy, please contact us at <a href="mailto:{email}?subject={domain}">{email}</a>.
+</p>
+</section>
---
+++ src/routes/terms.svelte
@@ -0,0 +1,52 @@
+<script>
+  const domain = 'SITE_DOMAIN';
+  const email = 'SITE_EMAIL';
+</script>
+
+<section>
+
+<h1>Terms of Service for {domain}</h1>
+
+<h2>1. Acceptance of Terms</h2>
+<p>
+  By accessing and using {domain}, you agree to comply with and be bound by these terms. If you do
+  not agree, please do not use the site.
+</p>
+
+<h2>2. Use of the Site</h2>
+<p>
+  You agree to use {domain} only for lawful purposes, and you must not use it in a way that infringes
+  the rights of others or restricts or inhibits anyone else's enjoyment of the site.
+</p>
+
+<h2>3. Content and Intellectual Property</h2>
+<p>
+  The content, design, and layout of {domain} are protected by intellectual property laws, including
+  copyright. You may not copy, reproduce, or distribute any part of the site without written consent.
+</p>
+
+<h2>4. Limitation of Liability</h2>
+<p>
+  We do not accept any responsibility or liability for any loss or damage that may arise from the
+  use of {domain}.
+</p>
+
+<h2>5. Changes to Terms</h2>
+<p>
+  We may revise these terms of service at any time. Please check this page regularly to take notice
+  of any changes we have made, as they are binding on you.
+</p>
+
+<h2>6. Governing Law</h2>
+<p>
+  These terms are governed by and construed in accordance with the laws of Santa Clara County,
+  California USA.
+</p>
+
+<h2>7. Contact Us</h2>
+<p>
+  If you have any questions about these terms, please contact us at <a
+    href="mailto:hello@numelli.com?subject=numelli.com">hello@numelli.com</a
+  >.
+</p>
+</section>
